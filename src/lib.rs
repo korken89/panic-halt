fee27944e98bf39e06b45292793ac15b513c00c3
Emil Fresk <emil.fresk@gmail.com>
2018-08-10 22:59:27 +0200

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+//! Set the panicking behavior to halt
+//!
+//! This crate contains an implementation of `panic_fmt` that simply halt in an infinite loop.
+//!
+//! # Usage
+//!
+//! ``` ignore
+//! #![no_std]
+//!
+//! extern crate panic_halt;
+//!
+//! fn main() {
+//!     panic!("argument is ignored");
+//! }
+//! ```
+
+#![deny(missing_docs)]
+#![deny(warnings)]
+#![feature(core_intrinsics)]
+#![feature(panic_implementation)]
+#![no_std]
+
+use core::panic::PanicInfo;
+use core::ptr;
+
+#[panic_implementation]
+fn panic(info: &PanicInfo) -> ! {
+    loop {
+        unsafe {
+            ptr::read_volatile(&info);
+        }
+    }
+}
